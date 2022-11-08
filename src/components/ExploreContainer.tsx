4375d1ea4cfd60c57a1ec04c279788eab71614c3
aparus <rustamapaev@gmail.com>
2022-11-09 03:17:25 +0400

    add audio pages

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,16 +1,32 @@
-import './ExploreContainer.css';
+import { useContext, useEffect, useLayoutEffect, useState } from 'react'
+import './ExploreContainer.css'
+import { SupabaseContext } from '../supabase/SupabaseProvider'
 
 interface ContainerProps {
-  name: string;
+	path: string
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-  return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
-};
+const ExploreContainer: React.FC<ContainerProps> = ({ path }) => {
+	const { supabase } = useContext(SupabaseContext)
 
-export default ExploreContainer;
+	const [mediaLink, setMediaLink] = useState('')
+
+	useLayoutEffect(() => {
+		const {
+			data: { publicUrl }
+		} = supabase.storage.from('audios').getPublicUrl(path)
+
+		setMediaLink(publicUrl)
+	}, [path])
+
+	return (
+		<div className='container'>
+			<strong>{path}</strong>
+			<div className='playerContainer'>
+				<audio controls src={mediaLink}></audio>
+			</div>
+		</div>
+	)
+}
+
+export default ExploreContainer
