77152fd54cb8c2934e21a206cfa60686299fd4b3
aparus <rustamapaev@gmail.com>
2023-01-05 10:32:15 +0400

    extract usePlayer from PlayerProvider

--- src/components/Player/Provider.tsx
+++ src/components/Player/Provider.tsx
@@ -1,7 +1,7 @@
-import { createContext, useContext, useEffect, useRef, useState } from 'react'
-import { AppStateContext } from '../AppStateProvider'
+import { createContext, useRef } from 'react'
+import usePlayer from './usePlayer'
 
-interface State {
+export interface State {
 	currentTime: number
 	currentTimePercentage: number
 	duration: number
@@ -11,13 +11,9 @@ interface State {
 	mediaLink: string
 }
 
-/* 
-type IonRangeEvent = {
-	detail: { value: number }
-} 
-*/
+// type IonRangeEvent = { detail: { value: number } }
 
-interface Methods {
+export interface Methods {
 	play: () => void
 	pause: () => void
 	plus5: () => void
@@ -26,6 +22,12 @@ interface Methods {
 	onRangeChange: (props: any) => void
 	onIonKnobMoveStart: (props: any) => void
 	onIonKnobMoveEnd: (props: any) => void
+}
+
+export interface EventHandlers {
+	onPlay: () => void
+	onPause: () => void
+	onTimeUpdate: () => void
 }
 
 interface ContextType {
@@ -37,109 +39,18 @@ interface Props {
 	children: JSX.Element
 }
 
-const playerInitialState = {
-	currentTime: 0,
-	duration: 0,
-	remainedTime: 0,
-	currentTimePercentage: 0,
-	isPlaying: false,
-	isSeeking: false,
-	mediaLink: ''
-}
-
 const defaultContextValue = {} as ContextType
 
 export const PlayerContext = createContext<ContextType>(defaultContextValue)
 
 const PlayerProvider: React.FC<Props> = ({ children }) => {
-	const [state, setState] = useState<State>(playerInitialState)
-
 	const mediaRef = useRef<HTMLMediaElement>(null)
 
 	const {
-		methods: { update: updateAppState }
-	} = useContext(AppStateContext)
-
-	useEffect(() => {
-		updateAppState({ loading: true })
-		if (mediaRef.current) {
-			mediaRef.current.onloadedmetadata = () => {
-				updateAppState({ loading: false })
-				setState(oldState => {
-					const { duration } = mediaRef.current!
-					return { ...oldState, duration }
-				})
-			}
-		}
-	}, [mediaRef, state.mediaLink])
-
-	// MEDIA ELEMENT EVENT HANDLERS
-
-	const onTimeUpdate = () => {
-		setState(oldState => {
-			const { currentTime, duration } = mediaRef.current!
-			const remainedTime = duration - currentTime || 0
-			const currentTimePercentage = (currentTime / duration) * 100 || 0
-			return { ...oldState, currentTime, remainedTime, currentTimePercentage }
-		})
-	}
-
-	const onPlay = () => {
-		setState(oldState => ({ ...oldState, isPlaying: true }))
-	}
-
-	const onPause = () => {
-		setState(oldState => ({ ...oldState, isPlaying: false }))
-	}
-
-	// ION-RANGE (SEEK BAR) EVENT HANDLERS
-
-	const onRangeChange = ({ detail: { value } }: any) => {
-		if (state.isSeeking) {
-			mediaRef.current!.currentTime = (value * state.duration) / 100
-		}
-	}
-
-	const onIonKnobMoveStart = ({ detail: { value } }: any) => {
-		setState(oldState => ({ ...oldState, isSeeking: true }))
-	}
-
-	const onIonKnobMoveEnd = ({ detail: { value } }: any) => {
-		setState(oldState => ({ ...oldState, isSeeking: false }))
-		mediaRef.current!.currentTime = (value * state.duration) / 100
-	}
-
-	// METHODS
-
-	const play = () => {
-		mediaRef.current!.play()
-	}
-
-	const pause = () => {
-		mediaRef.current!.pause()
-	}
-
-	const plus5 = () => {
-		mediaRef.current!.currentTime += 5
-	}
-	const minus5 = () => {
-		mediaRef.current!.currentTime -= 5
-	}
-
-	const setMediaLink = (mediaLink: string) => {
-		setState(oldState => ({ ...oldState, mediaLink }))
-	}
-
-	const methods = {
-		play,
-		pause,
-		plus5,
-		minus5,
-		setMediaLink,
-		onRangeChange,
-		onIonKnobMoveStart,
-		onIonKnobMoveEnd
-	}
+		state,
+		methods,
+		eventHandlers: { onPlay, onPause, onTimeUpdate }
+	} = usePlayer(mediaRef)
 
 	const contextValue = { state, methods }
 
---
+++ src/components/Player/usePlayer.tsx
@@ -0,0 +1,112 @@
+import { RefObject, useState, useContext, useEffect } from 'react'
+import { AppStateContext } from '../AppStateProvider'
+import { State, Methods, EventHandlers } from './Provider'
+
+const playerInitialState: State = {
+	currentTime: 0,
+	duration: 0,
+	remainedTime: 0,
+	currentTimePercentage: 0,
+	isPlaying: false,
+	isSeeking: false,
+	mediaLink: ''
+}
+
+const usePlayer = (mediaRef: RefObject<HTMLMediaElement>) => {
+	const [state, setState] = useState<State>(playerInitialState)
+
+	const {
+		methods: { update: updateAppState }
+	} = useContext(AppStateContext)
+
+	useEffect(() => {
+		updateAppState({ loading: true })
+		if (mediaRef.current) {
+			mediaRef.current.onloadedmetadata = () => {
+				updateAppState({ loading: false })
+				setState(oldState => {
+					const { duration } = mediaRef.current!
+					return { ...oldState, duration }
+				})
+			}
+		}
+	}, [mediaRef, state.mediaLink])
+
+	// MEDIA ELEMENT EVENT HANDLERS
+
+	const onTimeUpdate = () => {
+		setState(oldState => {
+			const { currentTime, duration } = mediaRef.current!
+			const remainedTime = duration - currentTime || 0
+			const currentTimePercentage = (currentTime / duration) * 100 || 0
+			return { ...oldState, currentTime, remainedTime, currentTimePercentage }
+		})
+	}
+
+	const onPlay = () => {
+		setState(oldState => ({ ...oldState, isPlaying: true }))
+	}
+
+	const onPause = () => {
+		setState(oldState => ({ ...oldState, isPlaying: false }))
+	}
+
+	// ION-RANGE (SEEK BAR) EVENT HANDLERS
+
+	const onRangeChange = ({ detail: { value } }: any) => {
+		if (state.isSeeking) {
+			mediaRef.current!.currentTime = (value * state.duration) / 100
+		}
+	}
+
+	const onIonKnobMoveStart = ({ detail: { value } }: any) => {
+		setState(oldState => ({ ...oldState, isSeeking: true }))
+	}
+
+	const onIonKnobMoveEnd = ({ detail: { value } }: any) => {
+		setState(oldState => ({ ...oldState, isSeeking: false }))
+		mediaRef.current!.currentTime = (value * state.duration) / 100
+	}
+
+	// METHODS
+
+	const play = () => {
+		mediaRef.current!.play()
+	}
+
+	const pause = () => {
+		mediaRef.current!.pause()
+	}
+
+	const plus5 = () => {
+		mediaRef.current!.currentTime += 5
+	}
+	const minus5 = () => {
+		mediaRef.current!.currentTime -= 5
+	}
+
+	const setMediaLink = (mediaLink: string) => {
+		setState(oldState => ({ ...oldState, mediaLink }))
+	}
+
+	const methods: Methods = {
+		play,
+		pause,
+		plus5,
+		minus5,
+		setMediaLink,
+		onRangeChange,
+		onIonKnobMoveStart,
+		onIonKnobMoveEnd
+	}
+
+	const eventHandlers: EventHandlers = {
+		onPlay,
+		onPause,
+		onTimeUpdate
+	}
+
+	return { state, methods, eventHandlers }
+}
+
+export default usePlayer
