ce35f7fa423f4213d52a1df1c796f2a1bbd180ef
aparus <rustamapaev@gmail.com>
2023-06-21 11:25:31 +0400

    refactor api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,70 +1,40 @@
 import axios from 'axios'
 import { TableOfContentType } from '../components/Navigation/types'
-import { LayerToDisplay } from '../components/AppStateProvider'
+import { Config, LayerToDisplay } from '../components/AppStateProvider'
 import { getDownloadURL, ref } from 'firebase/storage'
 import { storage } from '../firebase'
+import { Toc } from '../components/Navigation/NavigationProvider'
+
+export const getFile = async (path: string) => {
+	const link = await getDownloadURL(ref(storage, path))
+	return axios(link)
+		.then(result => result.data)
+		.catch(e => console.log(e))
+}
+
+export const getFiles = async (paths: string[]) => {
+	const tasks = paths.map(path => getFile(path))
+	const files = await Promise.all(tasks).catch(e => console.log(e))
+	return files
+}
 
 export const getTocs = async (layers: LayerToDisplay[]) => {
-	const tocLayers = layers.filter(
-		elem => elem.path.match('toc/') && elem.checked
-	)
-	const tocLinks = await Promise.all(
-		tocLayers.map(elem => {
-			const prms = getDownloadURL(ref(storage, elem.path + '.json')).catch(e =>
-				console.log(e)
-			)
-			return prms
-		})
-	)
+	const tocPaths = layers
+		.filter(elem => elem.path.match('toc/') && elem.checked)
+		.map(elem => elem.path + '.json')
 
-	const tocPromises = tocLinks.map(
-		elem => elem && axios.get<TableOfContentType>(elem)
-	)
+	const files = (await getFiles(tocPaths)) as TableOfContentType[]
 
-	const tocsRaw = await Promise.all(tocPromises)
-
-	const tocs = tocsRaw.map((elem, index) => {
-		const { data = [] } = elem || {}
+	const tocs = files.map((elem, index) => {
+		const data = elem
 		const info = layers[index]
 		return { info, data }
 	})
 
-	return tocs
-}
-
-export const getSubs = async (path: string, layers: LayerToDisplay[]) => {
-	const pathArray = path.split('/')
-	const [bookPath, ...rest] = pathArray
-	const pagePath = rest.join('/')
-	const { path: layerPath } = layers[0]
-	const { data: subs } = await axios(
-		`/content/${bookPath}/layers/${layerPath}/${pagePath}.txt`
-	)
-	return subs as string
-}
-
-export const getTranslation = async (path: string, trLang: string) => {
-	const pathArray = path.split('/')
-	const [bookPath, ...rest] = pathArray
-	const pagePath = rest.join('/')
-	const { data: translation } = await axios(
-		`/content/${bookPath}/layers/${trLang}/translation/${pagePath}.txt`
-	)
-	return translation as string
+	return tocs as Toc[]
 }
 
 export const getConfig = async () => {
-	const link = await getDownloadURL(ref(storage, 'config.json'))
-	const { data } = await axios(link)
-	return data
-}
-
-async function readFile(fileUrl: string) {
-	try {
-		const { data } = await axios(fileUrl)
-		return data
-	} catch (e) {
-		// console.log(e)
-		return ''
-	}
+	const config = (await getFile('config.json')) as Config
+	return config
 }
